0c2497efdc230cb65e0d0880b61d5faae960252a
L.Assis <laressap.assis@outlook.com>
2018-10-26 10:16:05 -0200

    La - Fim do Refactor do Login

--- SupplierRanking/Content/js/login.js
+++ SupplierRanking/Content/js/login.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
         sliderFornecedorComprador = $('.slider-fornecedor'),
         sliderFisicaJuridica = $('.slider-juridica'),
         sliderFuncionario = $('.slider-funcionario'),
-            
+
         inputCpf = $('#inputCpf'),
         inputCnpj = $('#inputCnpj'),
         inputFuncionario = $('#inputFuncionario'),
@@ -79,12 +79,12 @@ $(document).ready(function () {
         switchTipoFuncionario(switchFuncionario);
     });
 
+    // Primeira funcao a ser executada
     function init() {
         $('#wrapperFisicaJuridica').hide();
         $('#wrapperFuncionario').hide();
         $('.login-form').hide();
         inputFuncionario.hide();
-        //inputHidden.val(0);
     }
 
     // Funcao para tirar o slider da posicao neutra  
@@ -104,7 +104,6 @@ $(document).ready(function () {
             switchFuncionario.prop('checked', false);
             btnFuncionario.removeClass('active-switch');
         }
-        //slider.removeClass('unchecked');
     }
 
     // Funcao para aparecer / esconder entre Fornecedor ou Comprador
@@ -120,9 +119,9 @@ $(document).ready(function () {
             btnFornecedor.addClass('active-switch');
             $('#wrapperFuncionario').show();
             $('.login-form').show();
-            inputHidden.val('2');
             inputCpf.hide();
-            inputCnpj.show();
+            inputCnpj.mask('00.000.000/0000-00').show();
+            inputHidden.val('2');
 
         } else { //Seleciona o Comprador
 
@@ -168,10 +167,13 @@ $(document).ready(function () {
             btnFuncionario.removeClass('active-switch');
             switchFuncionario.prop('checked', false);
             inputFuncionario.hide();
+            $('.wrap-login').removeClass('has-employee');
+            inputHidden.val('2');
 
         } else {
             btnFuncionario.addClass('active-switch');
             switchFuncionario.prop('checked', true);
+            $('.wrap-login').addClass('has-employee');
             inputFuncionario.show();
             inputHidden.val('3');
         }
@@ -179,54 +181,5 @@ $(document).ready(function () {
         $('.login-form').show();
     }
 
-    /*==================================================================
-    [ Validate ]*/
-    //var input = $('.validate-input .input100');
-
-    //$('.validate-form').on('submit',function(){
-    //    var check = true;
-
-    //    for(var i=0; i<input.length; i++) {
-    //        if(validate(input[i]) == false){
-    //            showValidate(input[i]);
-    //            check=false;
-    //        }
-    //    }
-
-    //    return check;
-    //});
-
-
-    //$('.validate-form .input100').each(function(){
-    //    $(this).focus(function(){
-    //       hideValidate(this);
-    //    });
-    //});
-
-    //function validate (input) {
-    //    if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-    //        if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-    //            return false;
-    //        }
-    //    }
-    //    else {
-    //        if($(input).val().trim() == ''){
-    //            return false;
-    //        }
-    //    }
-    //}
-
-    //function showValidate(input) {
-    //    var thisAlert = $(input).parent();
-
-    //    $(thisAlert).addClass('alert-validate');
-    //}
-
-    //function hideValidate(input) {
-    //    var thisAlert = $(input).parent();
-
-    //    $(thisAlert).removeClass('alert-validate');
-    //}
-    
     init();
 });
